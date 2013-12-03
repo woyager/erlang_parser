dfd89ecbaf68136bdab740cedbccd10cbdd3041c
Maksim Anfilatov <maksim@woyager.ru>
2013-12-04 03:28:49 +0400

    full parsing for one line

--- parser_funcs.erl
+++ parser_funcs.erl
@@ -1,8 +1,33 @@
 -module(parser_funcs).
+-include("parser.hrl").
 -import(string,[tokens/2,str/2,substr/2,substr/3,len/1,concat/2,to_integer/1,to_float/1]).
 -import(lists,[reverse/1]).
 -import(re,[split/3,run/2]).
--export([parse_string/1]).
+-export([parse_string/1,parse_line/1]).
+
+parse_line(Str)->
+	PreParsed = parse_string(Str),
+	Upstream = split(PreParsed#parsed_line.upstream," : ",[{return,list}]),
+	UpstreamTime = split(PreParsed#parsed_line.upstreamtime," : ",[{return,list}]),
+	UpstreamCode = split(PreParsed#parsed_line.upstreamcode," : ",[{return,list}]),
+	send_to_next(PreParsed,Upstream,UpstreamTime,UpstreamCode)
+	.
+
+send_to_next(_Parsed,[],[],[])->[];
+send_to_next(Parsed,[HUp|Upstream],[HUpTime|UpstreamTime],[HUpCode|UpstreamCode])->
+	{CodeT,_}=to_integer(HUpCode),
+	{TimeT,_}=to_float(HUpTime),
+	Code = case CodeT of
+		error -> -1;
+		_ -> CodeT
+		end,
+	Time = case TimeT of
+		error -> -0.001;
+		_ -> TimeT
+		end,
+	P1 = Parsed#parsed_line{upstream=HUp,upstreamtime=Time*1000,upstreamcode=Code,resptime=Parsed#parsed_line.resptime*1000},
+	io:format("~p ~n",[P1]),
+	send_to_next(Parsed,Upstream,UpstreamTime,UpstreamCode).
 
 parse_string(Str)->
 	Items = split(concat(Str,"end"),"    ",[{return,list}]),
@@ -19,7 +44,7 @@ parse_string(Str)->
 		_ -> SizeT
 	       end,
 	RespTime = case RespTimeT of
-			error -> -1.0;
+			error -> -0.001;
 			_ -> RespTimeT
 		   end,
 		
@@ -33,42 +58,42 @@ parse_string(Str)->
 	Tstamp=calendar:datetime_to_gregorian_seconds({{Year,Mon,Date},{Hour,Min,Sec}})-62167219200,
 	{match,[_,{HostSt,HostSe}]}=run(Url,"http:\/\/(.*?)\/.*$"),
 	Host=substr(Url,HostSt+1,HostSe),
-	[
-		{ip,Ip},
-		{user,User},
-		{datetimetz,DateTimeTZ},
-		{date,Date},
-		{month,Mon},
-		{year,Year},
-		{hour,Hour},
-		{minutes,Min},
-		{seconds,Sec},
-		{tstamp,Tstamp},
-		{method,Method},
-		{host,Host},
-		{url,Url},
-		{version,Version},
-		{code,Code},
-		{size,Size},
-		{ref,Ref},
-		{agent,Agent},
-		{resptime,RespTime},
-		{upstreamtime,UpstreamTime},
-		{upstream,Upstream},
-		{cachestatus,CacheStatus},
-		{upstreamcode,UpstreamCode},
-		{memcachekey,MemcacheKey},
-		{enhancedmemcachekey,EnhancedMemcacheKey},
-		{backend,Backend},
-		{respcompl,RespCompl},
-		{contenttype,ContentType},
-		{requestid,RequestId},
-		{uidgot,UidGot},
-		{uidset,UidSet},
-		{farm,Farm},
-		{country,Country},
-		{hz,Hz}
-	].
+	#parsed_line{
+		ip=Ip,
+		user=User,
+		datetimetz=DateTimeTZ,
+		date=Date,
+		month=Mon,
+		year=Year,
+		hour=Hour,
+		minutes=Min,
+		seconds=Sec,
+		tstamp=Tstamp,
+		method=Method,
+		host=Host,
+		url=Url,
+		version=Version,
+		code=Code,
+		size=Size,
+		ref=Ref,
+		agent=Agent,
+		resptime=RespTime,
+		upstreamtime=UpstreamTime,
+		upstream=Upstream,
+		cachestatus=CacheStatus,
+		upstreamcode=UpstreamCode,
+		memcachekey=MemcacheKey,
+		enhancedmemcachekey=EnhancedMemcacheKey,
+		backend=Backend,
+		respcompl=RespCompl,
+		contenttype=ContentType,
+		requestid=RequestId,
+		uidgot=UidGot,
+		uidset=UidSet,
+		farm=Farm,
+		country=Country,
+		hz=Hz
+	}.
 
 monthToNum("Jan") -> 1;
 monthToNum("Feb") -> 2;
