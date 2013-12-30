6b01f261d2525278f6f4c9a73d360fa32a5c71ac
Maksim Anfilatov <maksim@woyager.ru>
2013-12-31 01:39:48 +0400

    fixes

--- parser_funcs.erl
+++ parser_funcs.erl
@@ -4,7 +4,15 @@
 -import(lists,[reverse/1]).
 -import(re,[split/3,run/2]).
 -import(file,[open/2,close/1,pread/3]).
--export([parse_string/1,parse_line/1,read_file/2,parse_message/0,parse_file/3]).
+-import(mongoapi,[recinfo/2]).
+-import(random,[uniform/0,uniform/1,seed/3,seed/0]).
+-export([parse_string/1,parse_line/2,read_file/2,parse_file/3,parse/3]).
+-include("erlmongo.hrl").
+
+parse(FileName,Start,End)->
+	application:start(erlmongo),
+	parse_file(FileName,Start,End)
+	.
 
 parse_file(FileName,Start,End) when End-Start>1000000 ->
 	spawn(parser_funcs,read_file,[FileName,Start]),
@@ -15,39 +23,39 @@ parse_file(FileName,Start,End)->
 read_file(FileName,LastPosition)->
 	{ok,File}=open(FileName,[read]),
 	{ok,Data}=pread(File,LastPosition,1000000),
+	Did = list_to_atom(concat(FileName,integer_to_list(LastPosition))),
+	mongodb:singleServer(Did),
+	mongodb:connect(Did),
+	Logs2 = mongoapi:new(Did,<<"logs2">>),
+	Logs2:recinfo(parsed_line,record_info(fields,parsed_line)),
 	DataStrings=split(Data,"\n",[{return,list}]),
-	StopAt=parse_data(FileName,0,DataStrings),
+	StopAt=parse_data(FileName,0,DataStrings,Logs2),
 	close(File),
 	StopAt.
 
-parse_data(FileName,LastPosition,[])->LastPosition;
-parse_data(FileName,LastPosition,[String|[]])->LastPosition;
-parse_data(FileName,LastPosition,[String|Strings])->
+parse_data(FileName,LastPosition,[],Mongo)->LastPosition;
+parse_data(FileName,LastPosition,[String|[]],Mongo)->LastPosition;
+parse_data(FileName,LastPosition,[String|Strings],Mongo)->
 %	io:format("String ~p ~n",[String]),
 %	io:format("LastPos: ~p ~n",[LastPosition]),
-%	parse_line(String),
+%	parse_line(String,Mongo),
 %	Pid = self(),
 %	Pid ! String,
-	Pid = spawn(parser_funcs,parse_message,[]),
-	Pid ! {log_string,String},
-	parse_data(FileName,LastPosition+len(String)+1,Strings).
+	seed(LastPosition,LastPosition,LastPosition),
+	Pid = spawn(parser_funcs,parse_line,[String,Mongo]),
+%	parse_line(String,Mongo),
+	parse_data(FileName,LastPosition+len(String)+1,Strings,Mongo).
 
-parse_message()->
-	receive
-		{log_string,String}->parse_line(String);
-		_Other -> {error}
-	end.
-
-parse_line(Str)->
+parse_line(Str,Mongo)->
 	PreParsed = parse_string(Str),
 	Upstream = split(PreParsed#parsed_line.upstream," : ",[{return,list}]),
 	UpstreamTime = split(PreParsed#parsed_line.upstreamtime," : ",[{return,list}]),
 	UpstreamCode = split(PreParsed#parsed_line.upstreamcode," : ",[{return,list}]),
-	send_to_next(PreParsed,Upstream,UpstreamTime,UpstreamCode)
+	send_to_next(PreParsed,Upstream,UpstreamTime,UpstreamCode,Mongo)
 	.
 
-send_to_next(_Parsed,[],[],[])->[];
-send_to_next(Parsed,[HUp|Upstream],[HUpTime|UpstreamTime],[HUpCode|UpstreamCode])->
+send_to_next(_Parsed,[],[],[],Mongo)->[];
+send_to_next(Parsed,[HUp|Upstream],[HUpTime|UpstreamTime],[HUpCode|UpstreamCode],Mongo)->
 	{CodeT,_}=to_integer(HUpCode),
 	{TimeT,_}=to_float(HUpTime),
 	Code = case CodeT of
@@ -60,7 +68,8 @@ send_to_next(Parsed,[HUp|Upstream],[HUpTime|UpstreamTime],[HUpCode|UpstreamCode]
 		end,
 	P1 = Parsed#parsed_line{upstream=HUp,upstreamtime=Time*1000,upstreamcode=Code,resptime=Parsed#parsed_line.resptime*1000},
 %	io:format("~p ~n",[P1]),
-	send_to_next(Parsed,Upstream,UpstreamTime,UpstreamCode).
+	Mongo:save(P1#parsed_line{docid=concat(P1#parsed_line.requestid,integer_to_list(uniform(1000000)))}),
+	send_to_next(Parsed,Upstream,UpstreamTime,UpstreamCode,Mongo).
 
 parse_string(Str)->
 	Items = split(concat(Str,"end"),"    ",[{return,list}]),
